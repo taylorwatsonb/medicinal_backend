bcc2f9b585d3fbb0e30288706df974659b73253a
github taylorwatsonb <github taylorwatsonb@gmail.com>
2020-05-23 04:54:10 -0400

    herb controller index and create routes

--- app/controllers/api/v1/herbs_controller.rb
+++ app/controllers/api/v1/herbs_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::HerbsController < ApplicationController
+    def index
+        @herbs = Herb.all
+        render json: @herbs
+      end
+    
+      def create
+        @herb.new(herb_params)
+        if @herb.save
+          render json: @herb, status: :accepted
+        else
+          render json: { errors: @herb.errors.full_messages }, status: :unprocessible_entity
+        end
+      end
+    
+      private
+    
+      def herb_params
+        params.permit(:title, :description, :image_url, :nature_id)
+      end
+    
 end
